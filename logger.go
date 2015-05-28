fe1acb3930164d1002bfd84cfe4682d6b24a38f6
Peter Olds <polds@kyanicorp.com>
2015-05-28 13:00:10 -0600

    Add a  "New()" function that allows for custom conf
    Signed-off-by: Peter Olds <polds@kyanicorp.com>

--- logger.go
+++ logger.go
@@ -10,6 +10,14 @@ import (
 
 var __l *logrus.Logger
 
+type Config struct {
+	Appname string
+	Host    string
+	Port    int
+
+	*logrus.Logger
+}
+
 // Logger returns an instance of
 // a logger or creates a new one.
 func Logger() *logrus.Logger {
@@ -20,26 +28,39 @@ func Logger() *logrus.Logger {
 	return __l
 }
 
-// NewLogger creates a new instances of a
-// logrus logger and returns the instance.
-func NewLogger() *logrus.Logger {
+// New creates a new instance of a
+// logger based on provided config data
+func New(config Config) Config {
+	config.Logger = logrus.New()
+	hook, err := logrus_papertrail.NewPapertrailHook(config.Host, config.Port, config.Appname)
+	hook.UseHostname()
+
+	// Register the PaperTrail hook
+	if err == nil {
+		config.Logger.Hooks.Add(hook)
+	}
+
+	return config
+}
+
+// DefaultConfig makes certain assumptions about your environment variables
+// and can be used to create a new basic instance.
+func DefaultConfig() Config {
 	app := os.Getenv("APPNAME")
 	if app == "" {
 		app, _ = os.Hostname()
 		os.Setenv("APPNAME", app)
 	}
 
-	host := os.Getenv("PAPERTRAIL_HOST")
 	port, _ := strconv.Atoi(os.Getenv("PAPERTRAIL_PORT"))
-
-	log := logrus.New()
-	hook, err := logrus_papertrail.NewPapertrailHook(host, port, app)
-	hook.UseHostname()
-
-	// Register the PaperTrail hook
-	if err == nil {
-		log.Hooks.Add(hook)
+	return Config{
+		Appname: os.Getenv("APPNAME"),
+		Host:    os.Getenv("PAPERTRAIL_HOST"),
+		Port:    port,
 	}
+}
 
-	return log
+// @TODO polds deprecate this function
+func NewLogger() *logrus.Logger {
+	return New(DefaultConfig()).Logger
 }
